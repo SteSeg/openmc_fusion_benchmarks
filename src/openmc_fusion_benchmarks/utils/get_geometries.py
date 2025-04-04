e3130ef3d93aebde925e6823a0a4a7d170620d5c
Stefano Segantin <segantin@psfc.mit.edu>
2025-04-04 17:52:49 -0400

    + download from drive function

--- src/openmc_fusion_benchmarks/utils/get_geometries.py
+++ src/openmc_fusion_benchmarks/utils/get_geometries.py
@@ -54,30 +54,30 @@ def get_cad_file(benchmark_name: str, cwd: str = ".") -> Path:
         shutil.rmtree(temp_clone_path, ignore_errors=True)
 
 
-# LIB_PATH = importlib.resources.files(
-#     "openmc_fusion_benchmarks.lib")
+LIB_PATH = importlib.resources.files(
+    "openmc_fusion_benchmarks.utils")
 
 
-# def download_from_drive(benchmark_name: str, file_format: str, run_option: str = None, cwd: str = None):
+def download_from_drive(benchmark_name: str, file_format: str, run_option: str = None, cwd: str = None):
 
-#     filepath = LIB_PATH / "cad_geometries.json"
-#     with open(filepath, "r") as f:
-#         data = json.load(f)
+    filepath = LIB_PATH / "cad_geometries.json"
+    with open(filepath, "r") as f:
+        data = json.load(f)
 
-#         # Your Google Drive file link
-#     if run_option is not None:
-#         url = data[benchmark_name][run_option][file_format]
-#     else:
-#         url = data[benchmark_name][file_format]
+        # Your Google Drive file link
+    if run_option is not None:
+        url = data[benchmark_name][run_option][file_format]
+    else:
+        url = data[benchmark_name][file_format]
 
-#     # Extract the file ID from the URL
-#     file_id = url.split("/d/")[1].split("/")[0]
-#     download_url = f"https://drive.google.com/uc?export=download&id={file_id}"
+    # Extract the file ID from the URL
+    file_id = url.split("/d/")[1].split("/")[0]
+    download_url = f"https://drive.google.com/uc?export=download&id={file_id}"
 
-#     # make sure cwd is identified as directory:
-#     if cwd is not None:
-#         if not cwd.endswith("/"):
-#             cwd += "/"
+    # make sure cwd is identified as directory:
+    if cwd is not None:
+        if not cwd.endswith("/"):
+            cwd += "/"
 
-#     # Download the file
-#     gdown.download(download_url, output=cwd, quiet=False, use_cookies=False)
+    # Download the file
+    gdown.download(download_url, output=cwd, quiet=False, use_cookies=False)
