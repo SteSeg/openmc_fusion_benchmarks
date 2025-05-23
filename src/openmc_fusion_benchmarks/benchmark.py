91aa79817189a74dfa6a8d1ed66e8baf331771b6
Stefano Segantin <92783079+SteSeg@users.noreply.github.com>
2025-05-23 17:27:00 -0400

    Update src/openmc_fusion_benchmarks/benchmark.py
    Co-authored-by: Ethan Peterson <eepeterson3@gmail.com>

--- src/openmc_fusion_benchmarks/benchmark.py
+++ src/openmc_fusion_benchmarks/benchmark.py
@@ -4,7 +4,6 @@ from abc import ABC, abstractmethod
 import numpy as np
 from .validate import validate_benchmark
 
-# Can be moved to OpenmcBenchmark constructor
 import openmc
 from cad_to_dagmc import CadToDagmc
 
