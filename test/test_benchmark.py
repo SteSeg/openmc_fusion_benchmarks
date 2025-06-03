3b56200e3cf01e3c2dcc0b376ec984dc2bbe8de6
Stefano Segantin <segantin@psfc.mit.edu>
2025-06-03 11:26:07 -0400

    fix test

--- test/test_benchmark.py
+++ test/test_benchmark.py
@@ -13,6 +13,7 @@ class DummyBenchmark(Benchmark):
     def _build_source(self): return "source"
     def _build_settings(self): return "settings"
     def _build_tallies(self): return "tallies"
+    def _build_model(self): return "model"
 
 
 @pytest.fixture
