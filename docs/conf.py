4c3178a6e3e609305f03b08cf020ed18f506b609
Stefano Segantin <segantin@psfc.mit.edu>
2025-07-07 17:45:17 -0400

    new buttons in docs

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,8 @@ html_theme_options = {
     "use_repository_button": True,
     "use_issues_button": True,
     "use_edit_page_button": True,
+    "use_fullscreen_button": True,
+    "use_version_picker": True,
     "path_to_docs": "docs",  # Path from root to docs folder
 }
 
