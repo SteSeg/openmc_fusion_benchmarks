f909bb1bf58fbda6de9f45aa53955e30bf3d7632
SteSeg <segantin@mit.edu>
2026-05-07 17:42:54 -0400

    + fixes

--- src/openmc_fusion_benchmarks/__init__.py
+++ src/openmc_fusion_benchmarks/__init__.py
@@ -3,6 +3,5 @@ from openmc_fusion_benchmarks.benchmark_results import *
 from openmc_fusion_benchmarks.validate_spec import *
 from openmc_fusion_benchmarks.validate_results import *
 from openmc_fusion_benchmarks.database import *
-import openmc_fusion_benchmarks.uq
 
 __version__ = "0.1.0"
