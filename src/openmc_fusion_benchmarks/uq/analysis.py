e91dad3139bfa54e651a0032c73b1c94573c8ebe
SteSeg <segantin@mit.edu>
2026-08-21 14:27:51 -0400

    + properties in pickfreezeanalysis

--- src/openmc_fusion_benchmarks/uq/analysis.py
+++ src/openmc_fusion_benchmarks/uq/analysis.py
@@ -8,3 +8,45 @@ class PickFreezeAnalysis:
             )
 
         self.tally = tally
+        self._validate()
+
+    def _validate(self):
+        A = self.tally.A
+        B = self.tally.B
+        AB = self.tally.AB
+
+        if A.shape != B.shape:
+            raise ValueError(
+                "Pick-freeze A and B ensembles must have the same shape."
+            )
+
+        if AB.shape[1] != A.shape[0]:
+            raise ValueError(
+                "Pick-freeze AB ensemble must have the same number "
+                "of realizations as A and B."
+            )
+
+    @property
+    def mean(self):
+        """Mean of the primary pick-freeze output ensemble."""
+        return self.tally.mean
+
+
+    @property
+    def variance(self):
+        """Variance of the primary pick-freeze output ensemble."""
+        return self.tally.variance
+
+
+    @property
+    def std_dev(self):
+        """Standard deviation of the primary pick-freeze output ensemble."""
+        return self.tally.std_dev
+
+    @property
+    def n_realizations(self):
+        return self.tally.A.shape[0]
+
+    @property
+    def n_perturbations(self):
+        return self.tally.AB.shape[0]
